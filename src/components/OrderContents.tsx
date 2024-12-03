14e6821216f8cc8b61a66677d68aafce5800e17f
Leo en laptop <leonar2zb@gmail.com>
2024-12-03 17:14:32 -0300

    Función helper para formato y estilizando y termiando vista

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../helpers"
 import { OrderItem } from "../types"
 
 type OrderContentsProps = {
@@ -8,13 +9,20 @@ export default function OrderContents({ order }: OrderContentsProps) {
     return (
         <div>
             <h2 className="font-black text-4xl">Consumo</h2>
-            <div className="space-y-3 mt-5">
+            <div className="space-y-3 mt-10">
                 {order.length === 0 ? <p className="text-center">La orden está vacía</p>
-                    : (order.map(item => (<div key={item.id}>
-                        <p >
-                            {item.name}
-                        </p>
-                    </div>)))}
+                    : (order.map(item => (
+                        <div className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b" key={item.id}>
+                            <div>
+                                <p className="text-lg">
+                                    {item.name} - {formatCurrency(item.price)}
+                                </p>
+                                <p className="font-black">
+                                    Cantidad: {item.quantity} - {formatCurrency(item.price * item.quantity)}
+                                </p>
+                            </div>
+                            <button className="bg-red-600 h-8 w-8 rounded-full text-white font-black">X</button>
+                        </div>)))}
             </div>
         </div>
     )
