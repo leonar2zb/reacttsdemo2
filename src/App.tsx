d500215d2df6eac458ed7de8e8d816572e6ab459
Leo en laptop <leonar2zb@gmail.com>
2024-12-03 12:32:28 -0300

    Dando estilos al menú

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import MenuItem from "./components/MenuItem"
 import { menuItems } from "./data/db"
 
 function App() {
@@ -7,6 +8,17 @@ function App() {
       <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de propinas</h1>
       </header>
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => <MenuItem key={item.id} item={item} />)}
+          </div>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,19 @@
+import type { MenuItem } from "../types"
+
+type MenuItemProps = {
+    item: MenuItem
+}
+export default function MenuItem({ item }: MenuItemProps) {
+    return (
+        <button
+        className="border-2 rounded-md border-teal-400 w-full p-3 hover:bg-teal-200 flex justify-between"
+        >
+            <p>
+                {item.name}
+            </p>
+            <p className="font-black">
+                ${item.price}
+            </p>
+        </button>
+    )
+}
